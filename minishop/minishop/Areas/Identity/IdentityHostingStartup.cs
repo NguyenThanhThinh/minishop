cdb73403d66f403d5e3ac5caf189236b3be2aec4
Thinh Nguyen <thanhthinhcntt@gmail.com>
2021-01-12 16:10:19 +0900

    add

---
+++ minishop/minishop/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Hosting;
+
+[assembly: HostingStartup(typeof(minishop.Areas.Identity.IdentityHostingStartup))]
+namespace minishop.Areas.Identity
+{
+
+	public class IdentityHostingStartup : IHostingStartup
+	{
+		public void Configure(IWebHostBuilder builder)
+		{
+			builder.ConfigureServices((context, services) =>
+			{
+			});
+		}
+	}
+}
+
